e36e511a0697cfa4a51d91fceb3390cc6dc1cc4a
Noah Rosenzweig <noah.rosenzweig@codecool.com>
2023-09-19 16:04:45 +0200

    Run scripts from file given as command-line argument

--- index.js
+++ index.js
@@ -1,20 +1,54 @@
+import fs from "node:fs/promises";
+import process, { stdin, stdout } from "node:process";
 import * as readline from "node:readline/promises";
-import { stdin, stdout } from "node:process";
-import { Context } from "oidascript/runner/context.js";
+import { run } from "oidascript";
+import { isError } from "oidascript/langError.js";
 import { parseStatement } from "oidascript/parser/statement.js";
+import { Context } from "oidascript/runner/context.js";
 import { runStatement } from "oidascript/runner/statement.js";
 
 const rl = readline.createInterface({ input: stdin, output: stdout });
 
 async function main() {
+    const arg = process.argv[2];
+
+    try {
+        if (arg && arg.trim().length > 0) {
+            await runFile(arg);
+        } else {
+            await runRepl();
+        }
+    } catch (e) {
+        if (isError(e)) {
+            console.error(e.display());
+        } else if (e instanceof Error) {
+            console.error(e);
+        } else {
+            console.error(e);
+        }
+    }
+
+    rl.close();
+}
+
+async function runFile(file) {
+    const content = await fs.readFile(file, "utf8").catch(() => {
+        throw new Error(`Failed to read file: ${file}`);
+    });
+
+    const result = run(content);
+    if (result.isErr()) {
+        throw result.getError();
+    }
+}
+
+async function runRepl() {
     const context = new Context();
 
     while (true) {
         const input = await rl.question("> ");
         runCode(input, context);
     }
-
-    rl.close();
 }
 
 function runCode(input, context) {
